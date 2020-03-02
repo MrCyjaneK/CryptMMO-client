978b4a84a76910ad14987775cacc911e917cde8a
Czarek Nakamoto <mrcyjanek@pm.me>
2020-03-02 19:59:19 +0100

    Added start script

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#You will most likely ran over issues with google logging in.
+#You need to add 'cryptmmo.mrcyjanek.net' to your /etc/hosts and point to '127.0.0.1'
+#Or change google auth keys, whatever.
+sudo PORT=80 npm start
+
