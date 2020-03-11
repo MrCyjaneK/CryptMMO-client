349c7ca554f13743d71e480ce23db57f5c71b4cf
Czarek Nakamoto <mrcyjanek@gmail.com>
2020-03-11 16:19:34 +0100

    Added <Profile /> function + some small changes

--- src/functions/Profile.js
+++ src/functions/Profile.js
@@ -1,43 +1,72 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { lighten, withStyles, makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import getUser from "./user.js";
+
+//TODO: Open popup menu when user clicks on attack, defense or dexterity and show stats like what items give that much power.
+
+
+const RedProgress = withStyles({
+    root: {
+        height: 10,
+        backgroundColor: lighten('#f44336', 0.75),
+    },
+    bar: {
+      borderRadius: 20,
+        backgroundColor: '#f44336',
+    },
+})(LinearProgress);
 
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
     },
     paper: {
-        height: 140,
-        width: 100,
+        height: theme.spacing(16),
+        width: theme.spacing(16),
     },
     control: {
         padding: theme.spacing(2),
     },
     avatar: {
-        width: theme.spacing(7),
-        height: theme.spacing(7)
+        width: theme.spacing(16),
+        height: theme.spacing(16)
     }
 }));
 
 export default function Profile(props) {
-    const [spacing, setSpacing] = React.useState(2);
     const classes = useStyles();
-    let user = user(props.userid)
+    let user = getUser(props.userid)
     return (
         <div>
-            <p>{JSON.stringify(props)}</p>
             <Grid item xs={12}>
-            <Grid container justify="center" spacing={spacing}>
-                <Grid key="profilePverview" item>
+            <Grid container justify="center" spacing={2}>
+                <Grid key="profilePreview" item>
                     <Paper className={classes.paper} >
-                        <Avatar alt="" >XD{/*This should be icon.. but #TODO*/}</Avatar>
+                        <Avatar className={classes.avatar} alt="" src="/images/profilepics/ninja.png">XD{/*This should be icon.. but #TODO*/}</Avatar>
                     </Paper>
+                    {/*This one is for HP*/}
+                    <RedProgress variant="determinate" value={user.hp} ></RedProgress>
                 </Grid>
+                <List component="nav" className={classes.userstats}>
+                    <ListItem button>
+                        <ListItemText primary={"Attack: "+user.attack} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary={"Defense: "+user.defense} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary={"Dexterity: "+user.dexterity} />
+                    </ListItem>
+                </List>
             </Grid>
         </Grid>
         </div>
--- src/functions/cache.js
+++ src/functions/cache.js
@@ -7,12 +7,15 @@
 //     Store everything in cache, and when needed load values from cache, and update them instantly.
 
 export function getCached(key) {
-	  key = key.split('.').join('_')
-	  if (localStorage[key]) {
-		     return localStorage[key]
-	  } else {
-	  	   return 0; // So if something is not set, or its 0, return 0
-	  }
+	key = key.split('.').join('_')
+	if (localStorage[key]) {
+        if (`${ localStorage[key] }` === "undefined") {
+            return 0;
+        }
+		return localStorage[key]
+	} else {
+	  	return 0; // So if something is not set, or its 0, return 0
+    }
 }
 export function storeCached(key, value) {
     key = key.split('.').join('_');
